227dddc7b781a17863dee61bf5f236c7a9d91ab3
Luis Junior <luiscarlossantossilvajunior@gmail.com>
2026-09-16 13:01:45 -0300

    reserva

--- Exams/Reserva.java
+++ Exams/Reserva.java
@@ -17,6 +17,48 @@ public class Reserva {
 		this.incluiRefeicoes = true;	
 	}
 	
+	public double calcularDiaria(){
+		return ((200 * this.diarias) + 
+		(this.diarias * (this.incluiRefeicoes ? 50 : 0)));
+	}
+	
+	public double calcularDiaria(double taxaLimpeza){
+		return ((200 * this.diarias) + 
+		(this.diarias * (this.incluiRefeicoes ? 50 : 0)) + taxaLimpeza);
+	}
+	
+	public double adicionarPasseio(String tipo){
+		if(tipo.equalsIgnoreCase("cavalgada"))
+			return 80.0;
+		else if(tipo.equalsIgnoreCase("trilha"))
+			return 60.0;
+		else if(tipo.equalsIgnoreCase("pesca"))
+			return 70.0;
+		return 0.0;
+	}
+	
+	public double adicionarPasseio(String tipo, int quantidade){
+		if(tipo.equalsIgnoreCase("cavalgada"))
+			return (80.0 * quantidade);
+		else if(tipo.equalsIgnoreCase("trilha"))
+			return (60.0 * quantidade);
+		else if(tipo.equalsIgnoreCase("pesca"))
+			return (70.0 * quantidade);
+		return 0.0;
+	}
+	
+	public String exibirReserva(){
+		return "Reserva [" + this.codigo + "] - Hospede: [" 
+		+ this.nomeHospede + "] - Diárias: [" + this.diarias + "]/n";
+	}
+	
+	public String exibirReserva(boolean comDetalhes){
+		return "Reserva [" + this.codigo + "] - Hospede: [" 
+		+ this.nomeHospede + "] - Diárias: [" + this.diarias + 
+		"] - Refeições inclusas: [" + (this.incluiRefeicoes ? "Sim" : "Não")
+		 + "]/n";
+	}
+		
 	public int getCodigo(){
 		return this.codigo;
 	}
